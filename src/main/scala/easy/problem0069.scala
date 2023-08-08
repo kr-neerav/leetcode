f5fcb6ba2d5ba06efa70d3675de890b6eadc10b8
Neerav Kumar <neeravk@amazon.com>
2023-08-07 21:28:44 -0700

    fixed probelm 69

--- src/main/scala/easy/problem0069.scala
+++ src/main/scala/easy/problem0069.scala
@@ -6,8 +6,12 @@ object Solution {
   }
 
   private def mySqrtGuessAlgorithm(guess: Int, x: Int): Int = {
-    if (guess == 0 || guess == x/guess) guess 
-    else mySqrtGuessAlgorithm((guess+x/guess)/2, x)
+    if (guess == 0) guess 
+    else {
+      val nextGuess = (guess + x/guess)/2
+      if (guess == nextGuess) guess 
+      else mySqrtGuessAlgorithm(nextGuess, x)
+    }
   }
 }
 
